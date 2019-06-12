db227d7fd017b151c5e11d31e91fe1e6dcc88550
Marcus Freire <vinny.freire@gmail.com>
2019-06-12 19:20:15 -0300

    DEPLOY 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Test App</header>
+      <header className="App-header">Test App xxx</header>
     </div>
   );
 }
