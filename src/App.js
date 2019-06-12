c23dccc26107cf05065489c005eab5d8436e825a
Marcus Freire <vinny.freire@gmail.com>
2019-06-12 19:06:48 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">teste</header>
+      <header className="App-header">Test App</header>
     </div>
   );
 }
